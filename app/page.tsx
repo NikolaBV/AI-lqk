ae8c43cfecb6791dfe71f5660c7f8bbe34760753
NikolaBV <bulgarianmapper64@gmail.com>
2024-03-24 11:20:22 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import OpenAI from "openai";
+export default async function Home() {
+  /*
+  const openai = new OpenAI();
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "Hi chatgpt!" }],
+    model: "gpt-3.5-turbo",
+  });
+  const response = completion.choices[0].message.content;
+*/
+  return <h1></h1>;
+}
