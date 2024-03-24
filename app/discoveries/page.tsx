cbdc9acf370c6be61ec8cd1233b82ddabc9d1c5c
NikolaBV <bulgarianmapper64@gmail.com>
2024-03-24 12:42:22 +0200

    OpenAI connected

--- app/discoveries/page.tsx
+++ app/discoveries/page.tsx
@@ -1,11 +1,21 @@
-export default function Page() {
-  /*
-  const openai = new OpenAI();
+import OpenAI from "openai";
+
+export default async function Page() {
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: "Hi chatgpt!" }],
     model: "gpt-3.5-turbo",
   });
-  const response = completion.choices[0].message.content;
-*/
-  return <h1>Discoveries</h1>;
+
+  const output = completion.choices[0].message.content;
+
+  return (
+    <div>
+      <h1>Start discovering</h1>
+      <p>{output}</p>
+    </div>
+  );
 }
